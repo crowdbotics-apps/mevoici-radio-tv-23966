ae857971d89308f90d1c132bc07fc28384a4d80c
Crowdbotics <team@crowdbotics.com>
2021-01-21 05:05:07 +0000

    Initial commit.

---
+++ ios/mevoici_radio_tv_23966Tests/mevoici_radio_tv_23966Tests.m
@@ -0,0 +1,30 @@
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+#import <React/RCTLog.h>
+#import <React/RCTRootView.h>
+
+#define TIMEOUT_SECONDS 600
+#define TEXT_TO_LOOK_FOR @"Welcome to React"
+
+@interface mevoici_radio_tv_23966 : XCTestCase
+
+@end
+
+@implementation mevoici_radio_tv_23966
+
+- (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
+{
+  if (test(view)) {
+    return YES;
+  }
+  for (UIView *subview in [view subviews]) {
+    if ([self findSubviewInView:subview matching:test]) {
+      return YES;
+    }
+  }
+  return NO;
+}
+
+
+@end
